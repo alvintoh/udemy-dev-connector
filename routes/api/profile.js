a37faaf135b561215931a91e7dbee58d8bffe9ca
Alvin Toh <alvin4292@gmail.com>
2019-10-21 18:08:07 +0800

    Added create Profile Action and Reducer

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,6 +21,8 @@ router.get('/me', auth, async (req, res) => {
     if (!profile) {
       return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+
+    res.json(profile);
   } catch (err) {
     console.error(err.messsage);
     res.status(500).send('Server Error');
