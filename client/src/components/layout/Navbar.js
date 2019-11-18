37349c7bd702ca8b8903f475eac3f47585a02090
Alvin Toh <alvin4292@gmail.com>
2019-11-18 11:14:30 +0800

    Finish adding Profile display to the codes and solving the console errors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,9 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/profiles'> Developers </Link>
+      </li>
+      <li>
         <Link to='/dashboard'>
           <i className='fas fa-user' />
           <span className='hide-sm'></span> Dashboard
@@ -26,7 +29,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <a href='#!'>Developers</a>
+        <Link to='/profiles'> Developers </Link>
       </li>
       <li>
         <Link to='/register'>Register</Link>
