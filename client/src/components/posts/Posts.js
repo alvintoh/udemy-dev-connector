810d6b634f1fcc55c9440b2f1e458b58ee02d23f
Alvin Toh <alvin4292@gmail.com>
2019-12-09 14:02:16 +0800

    Prepared for deployment

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,13 +2,30 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from '../posts/PostItem.js';
+import PostForm from '../posts/PostForm';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      <PostForm />
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
